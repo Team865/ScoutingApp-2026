173b145aae6fdff589ec2605dd7408f5b9db6a23
JackcN2 <jack.c.noble2@gmail.com>
2025-12-11 18:55:38 -0500

    Ip logging proxy fix

--- App.py
+++ App.py
@@ -4,10 +4,11 @@ import requests
 from dotenv import load_dotenv
 import geoip2.database
 import re
-
+from werkzeug.middleware.proxy_fix import ProxyFix
 load_dotenv()
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 API_KEY = os.getenv("TBA_API_KEY")
 ROOT_URL = "https://www.thebluealliance.com/api/v3"
