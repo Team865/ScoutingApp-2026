9d19933790dcbc0c5dce3c613d1ab6a1988782be
JackcN2 <jack.c.noble2@gmail.com>
2025-12-13 12:40:06 -0500

    Changed test mode to use a diffrent port

--- App.py
+++ App.py
@@ -185,10 +185,15 @@ def send_test_messages():
         
 
 #Put anything  that needs  to run only for testing here
-if  int(config["TEST_MODE"]) == True:
-    threading.Thread(target=send_test_messages, daemon=True).start()
 
-threading.Thread(target=poll_tba_matches, args=(appData, EVENT_KEY), daemon=True).start()
+    
+
+
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000, threads=16, trusted_proxy="127.0.0.1", trusted_proxy_headers=trusted_proxy_headers)
+    threading.Thread(target=poll_tba_matches, args=(appData, EVENT_KEY), daemon=True).start()
+    if  int(config["TEST_MODE"]) == True:
+        serve(app, host="0.0.0.0", port=5005, threads=16, trusted_proxy="127.0.0.1", trusted_proxy_headers=trusted_proxy_headers)
+        threading.Thread(target=send_test_messages, daemon=True).start()
+    else:
+        serve(app, host="0.0.0.0", port=5000, threads=16, trusted_proxy="127.0.0.1", trusted_proxy_headers=trusted_proxy_headers)
