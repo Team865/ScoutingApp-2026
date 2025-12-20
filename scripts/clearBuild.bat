90c171f5c2293860c66b992186ae6c409c7954bc
TVA018 <thomas.vu018@gmail.com>
2025-12-19 23:31:57 -0500

    Fix clearBuild script

--- scripts/clearBuild.bat
+++ scripts/clearBuild.bat
@@ -1,4 +1,3 @@
-WHERE .\static\build>nul 2>nul
 :: Delete build folder if it exists
-if %ERRORLEVEL% == 0 rmdir .\static\build /s /q
+if exist .\static\build\ (rmdir .\static\build /s /q)
 md .\static\build
