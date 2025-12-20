bcc8f981d4c46a4242be23fb7ab7c3b18284ba1c
TVA018 <thomas.vu018@gmail.com>
2025-12-20 14:10:08 -0500

    Fix error message showing when it shouldn't

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,9 +3,12 @@ echo ===BEGINNING BUILD PROCESS===
 
 call scripts\clearBuild.bat
 echo Build folder cleared
-call npx tsc || echo [91mTypeScript couldn't be compiled, did you make sure to run .\scripts\setup.bat beforehand?[0m
+call npx tsc
 
 if %ERRORLEVEL% == 0 (
     echo TypeScript Transpiled
     echo ===BUILD COMPLETE===
+) else if %ERRORLEVEL% == 1 (
+    :: Error Level of 1 means the command couldnt be run
+    echo [91mTypeScript Compiler couldn't be found, did you make sure to run .\scripts\setup.bat beforehand?[0m
 )
