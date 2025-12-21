bff0d52cd267fe204d10a2414669b354e32b5a33
TVA018 <thomas.vu018@gmail.com>
2025-12-21 11:44:01 -0500

    Swap from webpack to esbuild

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,9 +4,16 @@ echo ===BEGINNING BUILD PROCESS===
 call scripts\clearBuild.bat
 echo Build folder cleared
 
-call npx webpack -c .\webpack.config.js --stats errors-only
+call npx esbuild^
+ src/typescript/scouting/main.ts^
+ src/typescript/superscouting/main.ts^
+ src/typescript/analysis/main.ts^
+ --bundle^
+ --minify^
+ --sourcemap^
+ --outdir=static/build
 
 if %ERRORLEVEL% == 0 (
-    echo TypeScript Transpiled and Bundled with webpack
+    echo TypeScript Transpiled and Bundled with esbuild
     echo ===BUILD COMPLETE===
 )
