556a59b7cd537705ce243dc684f0b309eb6e76a4
TVA018 <thomas.vu018@gmail.com>
2026-01-08 21:46:59 -0500

    Allow signals to connect to other signals

--- src/typescript/lib/dataTypes/Signal.ts
+++ src/typescript/lib/dataTypes/Signal.ts
@@ -1,3 +1,5 @@
+type ConnectionTarget<EmitMessage> = ((emittedMessage: EmitMessage) => void) | Signal<EmitMessage>
+
 export default class Signal<EmitMessage> {
     private connectedFunctions: ((emittedMessage: EmitMessage) => void)[];
 
@@ -5,12 +7,26 @@ export default class Signal<EmitMessage> {
         this.connectedFunctions = [];
     }
 
-    public connect(functionToConnect: (emittedMessage: EmitMessage) => void) {
+    /** Connects a function or Signal to this Signal. 
+     * 
+     * If the `target` is a function, it will be called whenever this Signal emits a value.
+     * 
+     * If the `target` is a Signal, any emitted messages from this Signal will propagate to the target Signal.
+     * 
+     * @param target The `target` to connect this Signal to
+     * @returns A function which will disconnect the `target`
+     * */
+    public connect(target: ConnectionTarget<EmitMessage>) {
+        const functionToConnect: (emittedMessage: EmitMessage) => void = (target instanceof Signal) ? (target.emit.bind(target)) : target
+
         this.connectedFunctions.push(functionToConnect);
-        return {
-            disconnect: () => this.connectedFunctions.splice(
-                this.connectedFunctions.indexOf(functionToConnect), 1)
-        };
+        return (() => this.disconnect(functionToConnect));
+    }
+
+    public disconnect(target: ConnectionTarget<EmitMessage>) {
+        const functionToConnect: (emittedMessage: EmitMessage) => void = (target instanceof Signal) ? (target.emit) : target
+
+        this.connectedFunctions.splice(this.connectedFunctions.indexOf(functionToConnect), 1)
     }
 
     public emit(messageToEmit: EmitMessage) {
