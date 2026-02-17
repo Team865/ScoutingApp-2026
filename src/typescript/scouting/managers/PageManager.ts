1a3733c5e1826ddb651b98161dc5ae20149e0c0d
TVA018 <thomas.vu018@gmail.com>
2026-02-17 16:28:53 -0500

    Simplify scouting app

--- src/typescript/scouting/managers/PageManager.ts
+++ src/typescript/scouting/managers/PageManager.ts
@@ -2,8 +2,7 @@ import StartingShiftPrompt from "../components/StartingShiftPrompt";
 import AutoPage from "../pages/AutoPage";
 import EndgameClimbPage from "../pages/EndgameClimb";
 import EndgamePreClimbPage from "../pages/EndgamePreClimbPage";
-import OffShiftPage from "../pages/OffShiftPage";
-import OnShiftPage from "../pages/OnShiftPage";
+import ShiftsPage from "../pages/ShiftsPage";
 import Page from "../pages/Page";
 import TransitionPhasePage from "../pages/TransitionPhasePage";
 
@@ -11,13 +10,9 @@ const mainElement: HTMLElement = document.querySelector("main");
 
 const autoPage = new AutoPage();
 const transitionPhasePage = new TransitionPhasePage();
-const shiftPages: Page[] = [];
+const shiftsPage = new ShiftsPage();
 const endgamePreClimbPage = new EndgamePreClimbPage();
 const endgameClimbPage = new EndgameClimbPage();
-
-const startingShiftPrompt = new StartingShiftPrompt();
-
-const numShifts = 6; // Number of shifts (including both on- and off-shifts)
 
 let currentPage: Page = autoPage;
 
@@ -26,51 +21,17 @@ export namespace PageManager {
         autoPage.nextButton.addEventListener("click", _ => changePage(transitionPhasePage));
 
         transitionPhasePage.backButton.addEventListener("click", _ => changePage(autoPage));
-        transitionPhasePage.nextButton.addEventListener("click", _ => goToFirstShift());
+        transitionPhasePage.nextButton.addEventListener("click", _ => changePage(shiftsPage));
 
-        startingShiftPrompt.allianceConfirmed.connect(generateShifts);
+        shiftsPage.backButton.addEventListener("click", _ => changePage(transitionPhasePage));
+        shiftsPage.nextButton.addEventListener("click", _ => changePage(endgamePreClimbPage));
 
-        endgamePreClimbPage.backButton.addEventListener("click", _ => changePage(shiftPages.at(numShifts - 1)));
+        endgamePreClimbPage.backButton.addEventListener("click", _ => changePage(shiftsPage));
         endgamePreClimbPage.nextButton.addEventListener("click", _ => changePage(endgameClimbPage));
 
         endgameClimbPage.backButton.addEventListener("click", _ => changePage(endgamePreClimbPage));
 
         changePage(autoPage);
-    }
-
-    function generateShifts(isOnAllianceAtStart: boolean) {
-        let isOnAlliance = isOnAllianceAtStart;
-
-        for(let i = 0; i < numShifts; i++) {
-            const page = isOnAlliance ? new OnShiftPage() : new OffShiftPage();
-            shiftPages.push(page);
-
-            // Bind back button
-            if(i == 0) {
-                page.backButton.addEventListener("click", _ => changePage(transitionPhasePage));
-            } else {
-                page.backButton.addEventListener("click", _ => changePage(shiftPages.at(i - 1)))
-            }
-
-            // Bind next button
-            if(i + 1 < numShifts) {
-                page.nextButton.addEventListener("click", _ => changePage(shiftPages.at(i + 1)));
-                isOnAlliance = !isOnAlliance; // Flip mode
-            } else {
-                page.nextButton.addEventListener("click", _ => changePage(endgamePreClimbPage));
-            }
-        }
-
-        goToFirstShift();
-    }
-
-    function goToFirstShift() {
-        // Create shifts
-        if(shiftPages.length == 0) {
-            startingShiftPrompt.show();
-        } else {
-            changePage(shiftPages.at(0));
-        }
     }
 
     function changePage(targetPage: Page) {
--- src/typescript/scouting/pages/ShiftsPage.ts
+++ src/typescript/scouting/pages/ShiftsPage.ts
@@ -3,17 +3,17 @@ import FuelCounter from "../components/FuelCounter";
 import LabeledCheckbox from "../components/LabeledCheckbox";
 import Page from "./Page";
 
-export default class OnShiftPage extends Page {
+export default class ShiftsPage extends Page {
     public readonly backButton = document.createElement("button");
     public readonly nextButton = document.createElement("button");
 
     private readonly fuelCounter = new FuelCounter();
     private readonly intakeChoices = new CheckboxGroup("Intake", ["Depot", "Neutral Zone", "Outpost", "Opponent Alliance"]);
-    private readonly passerChoice = new LabeledCheckbox("Passer/Feeder?");
-    private readonly defenseChoice = new LabeledCheckbox("Defense?");
+    private readonly otherActions = new CheckboxGroup("Other Actions", ["Feeding/Passing", "Deposit to Human Player"]);
+    private readonly defenseChoices = new CheckboxGroup("Defense", ["Depot", "Outpost", "Trench", "Bump", "Other"]);
 
     constructor() {
-        super("ACTIVE HUB PERIOD");
+        super("TELEOP SHIFTS");
         this.backButton.textContent = "BACK";
         this.nextButton.textContent = "NEXT";
 
@@ -22,8 +22,8 @@ export default class OnShiftPage extends Page {
         this.domElement.append(
             this.fuelCounter.domElement,
             this.intakeChoices.domElement,
-            this.passerChoice.domElement,
-            this.defenseChoice.domElement
+            this.defenseChoices.domElement,
+            this.otherActions.domElement
         );
     }
 }
